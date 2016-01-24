33b25d70e6b477f3972a96edfc95ae33ffd264cc
Ethan Jackwitz <ethanjackwitz@gmail.com>
2016-01-25 08:13:57 +1000

    Added fill-non-dominated-sort

---
+++ NSGA-II-Tests/FillTests.swift
@@ -0,0 +1,68 @@
+//
+//  FillTests.swift
+//  NSGA-II
+//
+//  Created by Ethan Jackwitz on 1/19/16.
+//  Copyright © 2016 Ethan Jackwitz. All rights reserved.
+//
+
+import XCTest
+
+struct Point {
+	var x: UInt8
+	var y: UInt8
+}
+
+extension Point: CustomDebugStringConvertible {
+	var debugDescription: String {
+		return "x: \(x), y: \(y)"
+	}
+}
+
+extension Point: Hashable {
+	var hashValue: Int {
+		return Int("\(x)\(y)")!
+	}
+}
+
+func == (lhs: Point, rhs: Point) -> Bool {
+	return lhs.x == rhs.x && lhs.y == rhs.y
+}
+
+extension Point: Rankable {
+	func dominates(other: Point) -> Bool? {
+		var (flagOurs, flagTheirs) = (false, false)
+		
+		if self.x < other.x { flagOurs = true }
+		if self.x > other.x { flagTheirs = true }
+		if self.y < other.y { flagOurs = true }
+		if self.y > other.y { flagTheirs = true }
+
+		switch (flagOurs, flagTheirs) {
+		case (true, false): return true
+		case (false, true): return false
+		default: return .None
+		}
+	}
+}
+
+extension Point: CrowdingAssignable {
+	var obj: [Double] {
+		return [Double(x), Double(y)]
+	}
+}
+
+class FillTests: XCTestCase {
+
+	func testFill() {
+		
+		let points = [(1, 1), (0, 27), (27, 0), (20, 6), (14, 7), (10, 8), (9, 9), (8, 10), (7, 14), (6, 20), (13, 13)].map(Point.init)
+		let expectedOutput = Set([(1, 1), (0, 27), (27, 0), (6, 20), (20, 6), (14, 7), (7, 14)].map(Point.init))
+		
+  	let output = Set(best(7, from: points))
+		
+		XCTAssert(output == expectedOutput)
+		
+	}
+	
+}
---
+++ Source/Fill.swift
@@ -0,0 +1,36 @@
+//
+//  File.swift
+//  NSGA-II
+//
+//  Created by Ethan Jackwitz on 1/19/16.
+//  Copyright © 2016 Ethan Jackwitz. All rights reserved.
+//
+
+func best<U: protocol<Rankable, CrowdingAssignable>>(n: Int, from individuals: [U]) -> [U] {
+	guard individuals.count != n else { return individuals }
+	guard individuals.count > n else { fatalError() }
+	let dominance = assignDominance(individuals)
+	let fronts = assignFronts(dominance)
+	
+	var results: [U] = []
+	
+	var nMembers = 0
+	for front in fronts {
+		guard nMembers != n else { return results }
+		if nMembers + front.count <= n {
+			results += front
+			nMembers += front.count
+		} else {
+			
+			let sortedFront = zip(front, crowdingDistance(front)).sort({ $0.1.1 < $0.0.1 }).map({ $0.0 })
+			
+			for ind in sortedFront {
+				results.append(ind)
+				nMembers += 1
+				guard results.count != n else { return results }
+			}
+		}
+	}
+	
+	return results
+}
