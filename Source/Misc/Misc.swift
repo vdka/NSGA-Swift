6d6959817b75d0ec0a17e0872b9c580d80ff7f87
Ethan Jackwitz <ethanjackwitz@gmail.com>
2016-01-28 00:15:11 +1000

    Removed unused extensions from Misc

--- Source/Misc/Misc.swift
+++ Source/Misc/Misc.swift
@@ -9,62 +9,6 @@
 import Darwin
 
 extension Array {
-	subscript (index: UInt) -> Element {
-		return self[Int(index)]
-	}
-
-	/**
-	Randomly rearranges the elements of self using the Fisher-Yates shuffle
-	*/
-	mutating func shuffle () {
-
-		for i in (0..<count) {
-			let j = Int.random(0..<count)
-			guard j != i else { continue }
-			swap(&self[i], &self[j])
-		}
-
-	}
-
-	/**
-	Shuffles the values of the array into a new one
-
-	:returns: Shuffled copy of self
-	*/
-	func shuffled () -> Array {
-		var shuffled = self
-
-		shuffled.shuffle()
-
-		return shuffled
-	}
-
-	/**
-	Applies cond to each element in array, splitting it each time cond returns a new value.
-	
-	- parameter cond: Function which takes an element and produces an equatable result.
-	
-	- returns: Array partitioned in order, splitting via results of cond.
-	*/
-	func partitionBy <T: Equatable> (cond: (Element) -> T) -> [Array] {
-		var result = [Array]()
-		var lastValue: T? = nil
-
-		for item in self {
-			let value = cond(item)
-
-			if value == lastValue {
-				let index: Int = result.count - 1
-				result[index] += [item]
-			} else {
-				result.append([item])
-				lastValue = value
-			}
-		}
-
-		return result
-	}
-
 	subscript (safe index: Int) -> Generator.Element? {
 		return indices ~= index ? self[index] : nil
 	}
