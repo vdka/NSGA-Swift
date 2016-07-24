b584122d88e4ea2c8d0fe488c9e1e34298bca20d
Ethan Jackwitz <ethanjackwitz@gmail.com>
2016-07-24 17:39:53 +1000

    Cleaned up the run script

--- run.sh
+++ run.sh
@@ -2,4 +2,7 @@
 
 mkdir -p results
 
-./.build/release/NSGA $(pwd) $(od -vAn -N4 -tx4 < /dev/urandom | tr -d [:space:] ) 299 100
+GENERATIONS=299
+POPULATION=100
+
+./.build/release/NSGA $(pwd) $(od -vAn -N4 -tx4 < /dev/urandom | tr -d [:space:] ) $GENERATIONS $POPULATION
