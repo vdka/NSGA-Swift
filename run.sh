2ffa9a705a4009ccdbd3f638ffefb9b3ad6f71ef
Ethan Jackwitz <ethanjackwitz@gmail.com>
2016-11-23 14:52:48 +1000

    Made script include code in results dir

--- run.sh
+++ run.sh
@@ -5,6 +5,9 @@ mkdir -p results
 GENERATIONS=${1:-999}
 POPULATION=${2:-100}
 
+commit=`git rev-parse --short HEAD`
+branch=`git rev-parse --abbrev-ref HEAD`
+
 SEED=$(od -vAn -N4 -tx4 < /dev/urandom | tr -d [:space:] )
 
 echo "Using seed: $SEED"
@@ -13,4 +16,7 @@ echo "Using seed: $SEED"
 
 echo >> $(pwd)/results/$SEED/CONFIG.txt
 echo $(date +"%d-%m @ %H:%m") >> $(pwd)/results/$SEED/CONFIG.txt
+echo "$branch - $commit" >> $(pwd)/results/$SEED/CONFIG.txt
+
+zip -r results/code.zip . -x '*.build*' -x '*.git*' -x '*results*' -x '*.DS_Store*' > /dev/null
 
