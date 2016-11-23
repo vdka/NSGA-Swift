8659105f232662467af36014e9d8b085862ca3fb
Ethan Jackwitz <ethanjackwitz@gmail.com>
2016-11-23 23:30:04 +1000

    Added ability to read a SEED as 3rd parameter with run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+./build.sh
+
 mkdir -p results
 
 GENERATIONS=${1:-999}
@@ -8,15 +10,17 @@ POPULATION=${2:-100}
 commit=`git rev-parse --short HEAD`
 branch=`git rev-parse --abbrev-ref HEAD`
 
-SEED=$(od -vAn -N4 -tx4 < /dev/urandom | tr -d [:space:] )
+SEED=${3:-$(od -vAn -N4 -tx4 < /dev/urandom | tr -d [:space:] )}
 
 echo "Using seed: $SEED"
+
+mkdir -p results/$SEED
+
+zip -r results/$SEED/code.zip . -x '*Packages*' -x '*.build*' -x '*.git*' -x '*results*' -x '*.DS_Store*' > /dev/null
 
 ./.build/release/NSGA $(pwd) $SEED $GENERATIONS $POPULATION
 
 echo >> $(pwd)/results/$SEED/CONFIG.txt
 echo $(date +"%d-%m @ %H:%m") >> $(pwd)/results/$SEED/CONFIG.txt
 echo "$branch - $commit" >> $(pwd)/results/$SEED/CONFIG.txt
-
-zip -r results/code.zip . -x '*Packages*' -x '*.build*' -x '*.git*' -x '*results*' -x '*.DS_Store*' > /dev/null
 
